5dd5042452e0d81f098fb9bfdb05345bf8108997
786IqraAkram <iqra786akram@gmail.com>
2016-05-19 09:47:39 -0700

    Update client.py

--- client.py
+++ client.py
@@ -5,21 +5,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket Created'
          
 host = socket.gethostname()
-port = 5182                # Reserve a port for your service.
+port = 5188                # Reserve a port for your service.
 
 s.connect((host, port))
-data="hello\n"
-
-
- 
-#Bind socket to local host and port
-try:
-    s.sendall(data)
-except socket.error as msg:
-    print 'Bind failed. Error in Message sending!'
-    sys.exit()
-
-print 'Message Sent '
 print s.recv(1024)
 
 s.close                     # Close the socket when done
